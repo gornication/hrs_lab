e65fea148a8616fee89a320e71b24de12be58e2f
gornication <gorenkov3@gmail.com>
2024-05-30 17:57:37 +0700

    Update script

--- pl-script.groovy
+++ pl-script.groovy
@@ -1,5 +1,3 @@
-// pl-script.groovy
-
 def checkoutCode() {
     git 'https://github.com/gornication/hrs_lab.git'
 }
@@ -10,18 +8,22 @@ def syntaxCheck() {
 
 def buildDockerImage() {
     dockerImage = docker.build("${env.DOCKER_HUB_REPO}:${env.BUILD_NUMBER}")
+    echo "Docker Image built: ${env.DOCKER_HUB_REPO}:${env.BUILD_NUMBER}"
 }
 
 def pushDockerImage() {
     docker.withRegistry('https://index.docker.io/v1/', 'gornication-pipeline-lab-1') {
         dockerImage.push("${env.BUILD_NUMBER}")
         dockerImage.push("latest")
+        echo "Docker Image pushed: ${env.DOCKER_HUB_REPO}:${env.BUILD_NUMBER}"
     }
 }
 
 def deployToKubernetes() {
+    // Deploy to Kubernetes
     sh """
     kubectl set image deployment/your-deployment-name your-container-name=${env.DOCKER_HUB_REPO}:${env.BUILD_NUMBER}
     kubectl rollout status deployment/your-deployment-name
     """
+    echo "Deployment to Kubernetes completed"
 }
